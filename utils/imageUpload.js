7add5a1bf751827a0faddd75a105ded8f1753afc
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-08-31 21:25:47 +0600

    dur

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,9 +1,8 @@
 const multer = require("multer");
-const axios = require("axios");
 const FormData = require("form-data");
+const axios = require("axios");
 require("dotenv").config();
 
-// Use memory storage for Vercel serverless
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
@@ -23,10 +22,6 @@ const uploadImagesToCloudflare = async (req, res, next) => {
     const uploadedImages = [];
     const uploadedVariantImages = [];
 
-    const allFiles = [...(req.files.productImages || [])];
-    const variantFiles = [...(req.files.variantImages || [])];
-
-    // Helper function to upload a single file
     const uploadFile = async (file) => {
       const form = new FormData();
       form.append("file", file.buffer, file.originalname);
@@ -43,23 +38,19 @@ const uploadImagesToCloudflare = async (req, res, next) => {
           maxBodyLength: Infinity,
         }
       );
-
       return response.data.result.variants[0];
     };
 
-    // Upload product images
-    for (const file of allFiles) {
+    for (const file of req.files.productImages || []) {
       uploadedImages.push(await uploadFile(file));
     }
 
-    // Upload variant images
-    for (const file of variantFiles) {
+    for (const file of req.files.variantImages || []) {
       uploadedVariantImages.push(await uploadFile(file));
     }
 
     req.uploadedImageUrls = uploadedImages;
     req.uploadedVariantUrls = uploadedVariantImages;
-
     next();
   } catch (error) {
     console.error("Cloudflare upload error:", error.response?.data || error.message);
@@ -67,29 +58,4 @@ const uploadImagesToCloudflare = async (req, res, next) => {
   }
 };
 
-// Delete image from Cloudflare
-const deleteCloudflareImage = async (imageId) => {
-  try {
-    const response = await axios.delete(
-      `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v1/${imageId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGES_TOKEN}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.error(
-      "Error deleting image from Cloudflare:",
-      error.response ? error.response.data : error.message
-    );
-    throw error;
-  }
-};
-
-module.exports = {
-  multerUpload,
-  uploadImagesToCloudflare,
-  deleteCloudflareImage,
-};
+module.exports = { multerUpload, uploadImagesToCloudflare };
