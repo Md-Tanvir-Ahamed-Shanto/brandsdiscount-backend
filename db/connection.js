c20060dc5c25b39df3a7eebca866f02206e57556
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-08-31 20:56:22 +0600

    fix

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,5 @@
-import { execSync } from "child_process";
-import { PrismaClient } from "@prisma/client";
-
-if (process.env.NODE_ENV === "production") {
-  try {
-    execSync("npx prisma generate");
-    console.log("✅ Prisma Client regenerated at runtime");
-  } catch (err) {
-    console.error("❌ Prisma generate failed", err);
-  }
-}
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
-export default prisma;
+
+module.exports = prisma;
